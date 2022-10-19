ca76260f7ea21b346c06c453c0ec557877e5dc93
GPark <genius8343@gmail.com>
2022-10-19 10:32:16 +0900

    first commit

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ app.post('/signup', async (req, res) => {
   
   try {
     const { ggToken, email, name } = req.body;
-    const userId = uuidv1()
+    const mail = email.toLowerCase()
+    const userId = mail.split('@',1).join()
     const client = connect("tjrf7ngdzv5g", "57jbfp2gns73eta2en4h947tfwbk7fjnqjkbmsay9e4as5w22az5cpgjsafs84z2", "1215059")
     const timestamp = Math.floor(Date.now() / 1000) + (60 * 60)
     const token = client.createUserToken(userId,timestamp)
-    const mail = email.toLowerCase()
     res.status(200).json({ ggToken, token, mail, userId, token, name});
   } catch (error) {
     console.log(error);
